d5a5d13bb2eeae0d59aa315f42d2fcf5b82e03b1
gustavodakel <gustavodakelcunha@gmail.com>
2025-08-06 16:43:26 -0300

    produto simples

---
+++ ps.c
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+ 
+ int main ()
+ 
+ {
+   int vr1 = 0;
+   int vr2 = 0;
+   int vr3 = 0;
+ 
+ scanf("%d",&vr1);
+ 
+ scanf("%d",&vr2); 
+ 
+   vr3 = vr1 * vr2;
+   
+ printf("PROD = %d\n",vr3);
+  
+    return (0);
+  
+ }  
